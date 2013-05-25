5fa2ea2a63f0bddf17745104859b7a9944438d10
Daniel Friedman <danielnfriedman@gmail.com>
2013-05-25 16:44:57 +1000

    Support bitbucket, no longer require whole url

--- bin/install.js
+++ bin/install.js
@@ -7,72 +7,91 @@ var rmrf = require('rimraf')
 var path = require('path')
 var npm = require('npm')
 var fs = require('fs')
+var DESTINATION_PATH
+var REMOTE_LOCATION
+var EXTRACTED_PATH
 
 module.exports = function(program) {
 
   this.program = program;
   program
-    .command('install')
-    .usage('<github repo>')
+    .command('install <repository>')
+    .description('fetch and install a driver')
+    .option("-g, --github", "fetch from github (default)")
+    .option("-b, --bitbucket", "fetch from bitbucket")
     .action(fetch.bind(this));
 }
 
-function fetch(repo,dest) {
-  console.log('\x1b[36m','ninja','\x1b[0m','fetching driver');
+function fetch(repo,dest,opts) {
 
-  dest = determineDestination(dest);
+  opts = (typeof dest === 'object' && dest || opts)
+  dest = (typeof dest === 'string' && dest || process.cwd())
 
-  request(sanitize(repo) + '/archive/master.zip')
+  configure(repo,dest,opts);
+
+  console.log('\x1b[36m','ninja','\x1b[0m','fetching driver')
+
+  request(REMOTE_LOCATION)
     .pipe(unzip.Extract({ path: dest }))
-    .on("close", installDependencies.bind(this,repo,dest))
-    .on("error", function (er) {
-      console.error("Error retrieving driver",er)
-      process.exit(1);
+    .on("close", installDependencies)
+    .on("error", function (err) {
+      console.error('\x1b[31m','ninja','\x1b[0m','error', err)
+      process.exit(1)
     })
 }
 
-function determineDestination(dest) {
+function configure(repo,dest,opts) {
 
-  return typeof dest === 'string' && dest || process.cwd()
+  // bitbucket
+  if (opts.bitbucket) {
+
+    var sanitized = repo.replace(/https:\/\/bitbucket.org\/|\.git|\/?$/g, '')
+    var extractedFolder = fs.readdirSync(dest).filter(function(element){
+      return (element.indexOf(sanitized.replace('/','-')) === 0);
+    })
+
+    REMOTE_LOCATION   = 'https://bitbucket.org/' + sanitized + '/get/default.zip';
+    DESTINATION_PATH  = path.resolve(dest, path.basename(sanitized))
+    EXTRACTED_PATH    = path.resolve(dest, extractedFolder[0])
+
+    return
+  }
+
+  // default to github
+  var sanitized = repo.replace(/https:\/\/github.com\/|\.git|\/?$/g, '')
+  REMOTE_LOCATION = 'https://github.com/' + sanitized + '/archive/master.zip'
+  DESTINATION_PATH  = path.resolve(dest, path.basename(sanitized))
+  EXTRACTED_PATH    = DESTINATION_PATH + '-master'
 }
 
-function sanitize(repo) {
 
-  return repo.replace(/\.git|\/?$/g, '');
-}
+function installDependencies() {
 
-function installDependencies(repo,dest) {
-
-  console.log('\x1b[36m','ninja','\x1b[0m','installing dependencies');
-  // Github adds -master to the end of the directory unzipped
-  // TODO get the folder name from the unzip?
-  var driverPath = path.resolve(dest, path.basename(repo)) + '-master';
-  process.chdir(driverPath);
+  console.log('\x1b[36m','ninja','\x1b[0m','installing dependencies')
+  process.chdir(EXTRACTED_PATH);
 
   async.series([
     npm.load,
     npm.install,
-    moveIntoPlace.bind(this,driverPath)
+    moveIntoPlace
   ],function(err) {
 
     if (err) {
-      console.error('\x1b[31m','ninja','\x1b[0m','error',err);
+      console.error('\x1b[31m','ninja','\x1b[0m','error',err)
       process.exit(1)
     } else {
-      console.log('\x1b[32m','ninja','\x1b[0m','done');
+      console.log('\x1b[32m','ninja','\x1b[0m','done')
       process.exit(0)
     }
   })
 }
 
-function moveIntoPlace(driverPath,cb) {
+function moveIntoPlace(cb) {
 
-  console.log('\x1b[36m','ninja','\x1b[0m','moving into place');
+  console.log('\x1b[36m','ninja','\x1b[0m','moving into place')
 
-  var newPath = driverPath.replace(/-master?$/g,'');
-
-  rmrf(newPath,function(err){
-    if (err) return cb(err);
-    else fs.rename(driverPath, newPath, cb);
+  rmrf(DESTINATION_PATH, function(err) {
+    if (err) return cb(err)
+    else fs.rename(EXTRACTED_PATH, DESTINATION_PATH, cb)
   })
 }

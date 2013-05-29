e6b5c0a49ec8d612a6aa44263415529932d044e4
Tim <secoif@gmail.com>
2013-05-29 11:23:06 +0800

    install/uninstall shared utils. npm as subprocess
    npm takes over global error handling, leads to confusing error messages.
    converted to child process. this simplifies changing the cwd too.
    extracted log/error commands to utils.

---
+++ utils.js
@@ -0,0 +1,41 @@
+"use strict"
+
+var fs = require('fs')
+var path = require('path')
+
+/**
+ * Kill Ninja client. Assume it will be restarted, or we don't care if
+ * it isn't.
+ *
+ * @param {String} pidFile Path to pid
+ * @param {Function} cb Callback
+ * @api private
+ */
+
+exports.killClient = function killClient(program, cb) {
+  var pidFile = program.pid || ''
+  if (program.noKill || !pidFile) return cb()
+  fs.readFile(path.resolve(process.cwd(), pidFile), function(err, data) {
+    if (err) {
+      exports.log('could not read pid file %s', pidFile)
+      return cb()
+    }
+    var pid = parseInt(data)
+    if (pid) {
+      exports.log('killing client %d', pid)
+      process.kill(pid)
+    }
+    cb()
+  });
+}
+
+exports.log = function log(log) {
+  var args = [].slice.call(arguments, 1)
+  console.error.apply(console, ['\x1b[36m ninja \x1b[0m ' + log].concat(args))
+}
+
+exports.error = function error(err) {
+  var args = [].slice.call(arguments, 1)
+  console.error.apply(console, ['\x1b[31m ninja \x1b[0m error', err].concat(args))
+  process.exit(1)
+}

5c8b2621ead8658976cf2cd959826a4ccfc3518a
Tim <secoif@gmail.com>
2013-05-30 10:15:58 +0800

    Add log.error
    For cases where you want to log an error, but not process.exit

--- utils.js
+++ utils.js
@@ -36,9 +36,12 @@ exports.log = function log(log) {
   var args = [].slice.call(arguments, 1)
   console.error.apply(console, ['\x1b[36m ninja \x1b[0m ' + log].concat(args))
 }
-
-exports.error = function error(err) {
+exports.log.error = function error(err) {
   var args = [].slice.call(arguments, 1)
   console.error.apply(console, ['\x1b[31m ninja \x1b[0m error ' + err].concat(args))
+}
+
+exports.error = function error(err) {
+  exports.log.error.apply(null, arguments)
   process.exit(1)
 }

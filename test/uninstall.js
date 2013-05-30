9f9807fcd0c911af15b5f8444852dd4faace8636
Tim <secoif@gmail.com>
2013-05-30 08:32:37 +0800

    Add ninja uninstall test.

---
+++ test/uninstall.js
@@ -0,0 +1,45 @@
+"use strict"
+var exec = require('child_process').exec
+var spawn = require('child_process').spawn
+var assert = require('assert')
+var rmrf = require('rimraf')
+var fs = require('fs')
+describe('uninstall', function() {
+  before(function(done) {
+    this.timeout(60000) // wait a minute…
+    exec(__dirname + '/.././bin/ninja install https://github.com/nexxy/ninja-isight --driver-path /tmp/test', function(err) {
+      assert.ifError(err)
+      assert(fs.existsSync('/tmp/test/ninja-isight/package.json'))
+      done()
+    })
+  })
+
+  it('exists and displays usage', function(done) {
+    exec(__dirname + '/../bin/ninja uninstall --help', function(err, stdout) {
+      assert.ifError(err)
+      assert(/Usage: ninja-uninstall/gi.test(stdout))
+      done()
+    })
+  })
+
+  it('uninstalls and kills client when done', function(done) {
+    // spawn a pretend client that writes its pid to a file
+    // and waits to be closed
+    var dummyClientExitCode = undefined
+    var args = ['node', '-e', "var fs = require('fs'); fs.writeFileSync('/tmp/test/pid.pid', process.pid); setTimeout(function() {process.exit()}, 60000)"]
+    spawn('/usr/bin/env', args, {stdio: 'inherit'})
+    .on('error', done)
+    .on('close', function(code) {
+      dummyClientExitCode = code
+    })
+
+    spawn(__dirname + '/../bin/ninja', 'uninstall ninja-isight --driver-path /tmp/test --pid /tmp/test/pid.pid'.split(' '), {stdio: 'inherit'})
+    .on('error', done)
+    .on('close', function(code) {
+      assert(!fs.existsSync('/tmp/test/ninja-isight/package.json'))
+      assert.notStrictEqual(0, dummyClientExitCode) // if we kill it, it won't exit 0
+      assert.strictEqual(0, code)
+      done()
+    })
+  })
+})

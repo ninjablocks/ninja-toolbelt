044e5f7cc230108b8be8a7a756a3563e0d21c4a2
Tim <secoif@gmail.com>
2013-06-06 16:34:12 +0800

    Add hasinternet tool.

---
+++ test/internet.js
@@ -0,0 +1,29 @@
+"use strict"
+
+var assert = require('assert')
+var internet = require('../lib/internet')
+var isme = require('isme')
+var net = require('net')
+
+// how to test this properly?
+// auto turn off the network during testing? ugh.
+describe('has internet', function() {
+  it("true if the machine has an internet connection", function(done) {
+    internet(function(err, hasInternet) {
+      // ignore err, it will be truthy if there is no internet
+      // (e.g. ENOTFOUND)
+      var connection = net.connect({port: 80, host: 'google.com'}, function() {
+        assert.ok(hasInternet)
+        connection.end()
+        done()
+      })
+
+      connection.on('error', function() {
+        assert.ok(!hasInternet)
+        connection.end()
+        done()
+      })
+    })
+  })
+})
+

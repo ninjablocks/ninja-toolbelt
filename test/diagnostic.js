2eaf6512f84c11cfad4c4fede5803b4db0ed152a
Tim <secoif@gmail.com>
2013-07-07 12:45:40 +0800

    Allow library usage.

--- test/diagnostic.js
+++ test/diagnostic.js
@@ -8,36 +8,52 @@ var concat = require('concat-stream')
 var Diagnostics = require('../lib/diagnostic')
 
 describe('ninja diagnostic', function() {
-  it('does all diagnostics when not supplied a name', function(done) {
-    var child = spawn(__dirname + '/../bin/ninja-diagnostic')
-    child.stdout.setEncoding('utf8')
-    child.stdout.pipe(concat(function(data) {
-      Object.keys(Diagnostics.list).forEach(function(title){
-        assert.ok(data.indexOf(title + ':') !== -1) // check for headers
-      })
-      assert.ok(data)
-    }))
-    child.on('exit', function(code) {
-      assert.strictEqual(code, 0)
-      done()
+  describe('lib', function() {
+    it('can take streams for stdout, stderr', function(done) {
+      Diagnostics('uname', {
+        stdout: concat(function(data) { // stdout
+          assert.ok(data)
+        }),
+        stderr: concat(function(data) { // stderr
+          assert.ok(!data) // should be no stderr
+        })
+      }, done)
     })
   })
-  it('runs a single diagnostic when passed a name', function(done) {
-    var list = Object.keys(Diagnostics.list)
-    var name = list[0]
-    assert.ok(name, "No diagnostics?")
-    var child = spawn(__dirname + '/../bin/ninja-diagnostic', [name])
-    child.stdout.setEncoding('utf8')
-    child.stdout.pipe(concat(function(data) {
-      assert.ok(data.indexOf(name + ':') !== -1) // check for headers
-      list.slice(1).forEach(function(title){
-        assert.ok(data.indexOf(title + ':') === -1) // check for headers
+
+  describe('bin', function() {
+    it('does all diagnostics when not supplied a name', function(done) {
+      var child = spawn(__dirname + '/../bin/ninja-diagnostic')
+      child.stdout.setEncoding('utf8')
+      child.stdout.pipe(concat(function(data) {
+        Object.keys(Diagnostics.list).forEach(function(title){
+          assert.ok(data.indexOf(title + ':') !== -1, "Missing header: " + title) // check for headers
+        })
+        assert.ok(data)
+      }))
+      child.on('exit', function(code) {
+        assert.strictEqual(code, 0)
+        done()
       })
-      assert.ok(data)
-    }))
-    child.on('exit', function(code) {
-      assert.strictEqual(code, 0)
-      done()
+    })
+
+    it('runs a single diagnostic when passed a name', function(done) {
+      var list = Object.keys(Diagnostics.list)
+      var name = list[0]
+      assert.ok(name, "No diagnostics?")
+      var child = spawn(__dirname + '/../bin/ninja-diagnostic', [name])
+      child.stdout.setEncoding('utf8')
+      child.stdout.pipe(concat(function(data) {
+        assert.ok(data.indexOf(name + ':') !== -1, "Missing header: " + name) // check for headers
+        list.slice(1).forEach(function(title){
+          assert.ok(data.indexOf(title + ':') === -1, "Missing header: " + title) // check for headers
+        })
+        assert.ok(data)
+      }))
+      child.on('exit', function(code) {
+        assert.strictEqual(code, 0)
+        done()
+      })
     })
   })
 })
